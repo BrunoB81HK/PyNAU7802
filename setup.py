cbd662d6f7865ef002bf2a6d90a57f2058d5c937
BrunoB81HK <50412835+BrunoB81HK@users.noreply.github.com>
2020-12-16 09:47:47 -0500

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='PyNAU7802',
-    version='0.1.0',
+    version='0.2.0',
     author='Bruno-Pier Busque',
     author_email='bruno-pier.busque@usherbrooke.ca',
     description="Python port of the SparkFun Qwiic Scale NAU7802 Arduino Library",
